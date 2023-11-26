c3c70fab6e18e6ecbbc0d6ce138665cadb08f0b0
prathmesh988 <prathmeshgawade41@gmail.com>
2023-11-26 12:19:51 +0530

    saving the stream to the cloud server

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,14 @@
 const NodeMediaServer = require('node-media-server');
 const ffmpeg = require('fluent-ffmpeg');
 const fs = require('fs');
+const path = require('path');
+const{spawn} = require('child_process');
+
+const ffmpegPath = ffmpeg.setFfmpegPath('C:\\Program Files\\ffmpeg-2023-11-22-git-0008e1c5d5-full_build\\bin'); // Path to the bundled ffmpeg
+// const ffmpegExecutable = path.basename(ffmpegPath); // Extracting the executable name
+const ffmpegExecutablePath = path.join('C:\\Program Files\\ffmpeg-2023-11-22-git-0008e1c5d5-full_build\\bin', 'ffmpeg.exe');
+ 
+console.log('ffmpeg executable path:', ffmpegExecutablePath);
 
 const config = {
   rtmp: {
@@ -16,9 +24,33 @@ const config = {
     allow_origin: '*',
   },
 };
+function startRecording(inputURL, outputFilePath) {
+  const ffmpegProcess = spawn(ffmpegExecutablePath, [
+    '-i', inputURL,
+    '-c', 'copy',
+    '-f', 'flv',
+    outputFilePath,
+  ]);
 
+  ffmpegProcess.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  ffmpegProcess.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  ffmpegProcess.on('close', (code) => {
+    console.log(`Child process exited with code ${code}`);
+  });
+}
 const nms = new NodeMediaServer(config);
 // Add a new onConnect hook to handle incoming RTMP streams
+nms.on('onConnect', (id, args) => {     
+  console.log('hello world who asked?')
+  
+
+});
 
 nms.on('onMessage', (id, msg) => {
   console.log(`Received message from client ${id}: ${msg}`);
@@ -29,44 +61,64 @@ nms.on('onMessage', (id, msg) => {
   // You can respond to the message or perform any other actions here
 });
 
-nms.on('onConnect', (id, args) => {
-  console.log('RTMP server is running on port 1935');
+nms.on('preConnect', (id, args) => {
+  console.log('[NodeEvent on preConnect]:', `id=${id} args=${JSON.stringify(args)}`);
   console.log(`New RTMP stream connected: ${args.app}/${args.stream}`);
 
   // Set the output file path for saving the encoded stream
   const outputFilePath = `./output/${args.stream}_encoded.mp4`;
 
-  // Perform video encoding for the incoming stream
-  const ffmpegProcess = ffmpeg()
-    .input(`rtmp://localhost:1935/${args.app}/${args.stream}`)
-    .inputFormat('flv')
-    .videoCodec('libx264')
-    .audioCodec('aac')
-    .format('flv') // Keep the output format as FLV for RTMP streaming
-    .outputOptions([
-      '-preset ultrafast',
-      '-tune zerolatency',
-    ])
-    .output(outputFilePath)
-    .on('end', () => console.log(`Video encoding for stream ${args.stream} complete`))
-    .on('error', (err) => console.error(`Error for stream ${args.stream}:`, err))
-    .run();
+  // perform a video encoding after that save that file to the output folder which is present in the current directory using ffmpeg
+  // hello world hwho aaksed?
+  startRecording
+  startRecording(`rtmp://localhost:1935/live/hello`, outputFilePath);
 
-  // Add cleanup logic when the connection ends
-  nms.on('onClose', (id, args) => {
-    if (args.app === 'live' && args.stream === args.stream) {
-      console.log(`RTMP stream ${args.stream} closed`);
-      ffmpegProcess.kill('SIGTERM'); // Stop ffmpeg process when the stream disconnects
-    }
-  });
+  console.log('stream is sended everytime when the client connects to the server');
+  
+});
+// nms.on('postPublish', (id, streamPath, args) => {
+//   console.log(`Stream connected: ${streamPath}`);
+//   // Your custom logic after a stream is connected goes here
+//   const ffmpegCommand = spawn('ffmpeg', [
+//     '-i', `rtmp://localhost:1935${streamPath}`, // input RTMP stream URL
+//     '-c', 'copy',
+//     '-f', 'flv',
+//     'output.flv', // replace with your desired output file name
+//   ]);
 
-  nms.on('onMessage', (id, msg) => { 
-    console.log(`Received message from client ${id}: ${msg}`);
-    // You can respond to the message or perform any other actions here
-  } );
+//   ffmpegCommand.stdout.on('data', (data) => {
+//     console.log(`FFmpeg stdout: ${data}`);
+//   });
+
+//   ffmpegCommand.stderr.on('data', (data) => {
+//     console.error(`FFmpeg stderr: ${data}`);
+//   });
+
+//   ffmpegCommand.on('close', (code) => {
+//     console.log(`FFmpeg process closed with code ${code}`);
+//   });
+// });
+
+
+nms.on('onClose', (id, args) => {
+  console.log(`RTMP stream ${args.stream} closed`);
+
+  const ffmpegProcess = ffmpegProcesses.get(args.stream);
+  if (ffmpegProcess) {
+    ffmpegProcess.kill('SIGTERM');
+    ffmpegProcesses.delete(args.stream);
+  }
+
+  // Additional logic for stream closure can be added here
 });
 
-nms.run();
+nms.on('onMessage', (id, msg) => {
+  console.log(`Received message from client ${id}: ${msg}`);
+  // Additional logic for handling messages can be added here
+});
+
+
+
 
 nms.on('onClose', (id, args) => {
   if (args.app === 'live' && args.stream === args.stream) {
@@ -107,6 +159,6 @@ nms.on('onClose', (id, args) => {
     uploadRequest.end();
   }
 });
-
+nms.run();
 console.log('RTMP server is running on port 1935');
 console.log('HTTP server is running on port 8000');
